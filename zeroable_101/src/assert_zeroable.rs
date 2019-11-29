059047eeeff7dbfe7759156e63f577cda6ccf19f
rodrimati1992 <rodrimatt1985@gmail.com>
2019-11-29 19:30:56 -0300

    Impld the cmp traits,Hash manually for AssertZeroable,ran cargo fmt on file.

--- zeroable_101/src/assert_zeroable.rs
+++ zeroable_101/src/assert_zeroable.rs
@@ -1,52 +1,88 @@
 use crate::Zeroable;
 
+use bytemuck::Pod;
+
 use core::{
-    fmt::{self,Debug},
+    cmp::{Eq, Ord, Ordering, PartialEq, PartialOrd},
+    fmt::{self, Debug},
+    hash::{Hash, Hasher},
     marker::PhantomData,
     mem,
 };
 
-
 /// A value level assertion that `T:Zeroable`.
-#[derive(Eq,PartialEq,Ord,PartialOrd,Hash)]
-pub struct AssertZeroable<T:?Sized>(PhantomData<T>);
+pub struct AssertZeroable<T: ?Sized>(PhantomData<T>);
 
+impl<T> AssertZeroable<T>
+where
+    T: Zeroable + ?Sized,
+{
+    /// Constructs a `AssertZeroable<T>`
+    pub const NEW: Self = AssertZeroable(PhantomData);
+}
 
-impl<T:?Sized> Copy for AssertZeroable<T>{}
+impl<T> AssertZeroable<T> {
+    /// Gets a zeroed `T`.
+    ///
+    /// This is safe to call,
+    /// since constructing a `AssertZeroable<T>``requires proving that `T` is `Zeroable`.
+    #[inline(always)]
+    pub fn zeroed(self) -> T {
+        unsafe { mem::zeroed() }
+    }
+}
 
-impl<T:?Sized> Clone for AssertZeroable<T>{
-    fn clone(&self)->Self{
+unsafe impl<T> Zeroable for AssertZeroable<T> where T: Zeroable {}
+
+unsafe impl<T> Pod for AssertZeroable<T> where T: Pod {}
+
+impl<T: ?Sized> Copy for AssertZeroable<T> {}
+
+impl<T: ?Sized> Clone for AssertZeroable<T> {
+    fn clone(&self) -> Self {
         *self
     }
 }
 
-impl<T:?Sized> Debug for AssertZeroable<T>{
-    fn fmt(&self,f:&mut fmt::Formatter<'_>)->fmt::Result{
-        let mut ds=f.debug_struct("AssertZeroable");
-        #[cfg(feature="print_type")]
-        let ds=ds.field("type",&std::any::type_name::<T>());
-        ds.finish()
+impl<T: ?Sized> PartialEq for AssertZeroable<T> {
+    #[inline]
+    fn eq(&self, _other: &Self) -> bool {
+        true
     }
 }
 
+impl<T: ?Sized> Eq for AssertZeroable<T> {}
 
-impl<T> AssertZeroable<T>
-where
-    T:Zeroable+?Sized
-{
-    /// Constructs a `AssertZeroable<T>`
-    pub const NEW:Self=AssertZeroable(PhantomData);
+impl<T: ?Sized> PartialOrd for AssertZeroable<T> {
+    #[inline]
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
 }
 
-impl<T> AssertZeroable<T>{
-    /// Gets a zeroed `T`.
-    /// 
-    /// This is safe to call,
-    /// since constructing a `AssertZeroable<T>``requires proving that `T` is `Zeroable`.
-    #[inline(always)]
-    pub fn zeroed(self)->T{
-        unsafe{
-            mem::zeroed()
-        }
+impl<T: ?Sized> Ord for AssertZeroable<T> {
+    #[inline]
+    fn cmp(&self, _other: &Self) -> Ordering {
+        Ordering::Equal
+    }
+}
+
+impl<T: ?Sized> Hash for AssertZeroable<T> {
+    fn hash<H>(&self, state: &mut H)
+    where
+        H: Hasher,
+    {
+        ().hash(state)
+    }
+}
+
+impl<T: ?Sized> Debug for AssertZeroable<T> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let mut ds = f.debug_struct("AssertZeroable");
+
+        #[cfg(feature = "print_type")]
+        let ds = ds.field("type", &std::any::type_name::<T>());
+
+        ds.finish()
     }
 }
